b8fb214ea9a6024527f0a8c5237d9ff6a2f623a1
zvank <nszvonkov@edu.hse.ru>
2024-04-05 12:56:58 +0300

    1) Fix a bug in parser. The wrong data block was parsed. 2) Add colunm 'num_blocks', containing the amount of data blocks in result.log file (corresponds directly to the span of the simulation in months.

--- parser/block_finder.py
+++ parser/block_finder.py
@@ -1,24 +1,43 @@
 import re
 
-def find_block_with_min_lines(file_path, regexes = [
+def find_last_block_with_min_lines(file_path, regexes = [
         r'\|(.+?)\|(.+?)\|', 
         r'\+(\=+)\+',
         r'\+[\-=]+\+'
     ], min_lines=20):
     with open(file_path, 'r', encoding='utf-8') as file:
+        lines = file.readlines()[::-1]  # Reverse the lines
         block = []
         lines_count = 0
-        for line in file:
+        found_last_block = False
+        for line in lines:
             if any(re.match(regex, line) for regex in regexes):
                 block.append(line)
             else:
-                lines_count = len(block)
-                if lines_count >= min_lines:
-                    return ''.join(block)
+                if found_last_block and len(block) >= min_lines:
+                    return ''.join(block[::-1])  # Reverse the block to its original order
+                if len(block) >= min_lines:
+                    found_last_block = True
                 block = []  # Reset block if a line that doesn't match is encountered
-        # Check if the last block meets the criteria
-        lines_count = len(block)
-        if lines_count >= min_lines:
-            return ''.join(block)
-    print(f"Warning: didn't found viable data block in .log file. Path: {file_path}")
-    return None  # Return None if no block with the specified minimum lines is found
+            lines_count = len(block)
+        return None  # Return None if no second last block with the specified minimum lines is found
+
+def count_data_blocks_with_min_lines(file_path, regexes = [
+        r'\|(.+?)\|(.+?)\|', 
+        r'\+(\=+)\+',
+        r'\+[\-=]+\+'
+    ], min_lines=20):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        block_count = 0
+        lines_in_block = 0
+        for line in file:
+            if any(re.match(regex, line) for regex in regexes):
+                lines_in_block += 1
+            else:
+                if lines_in_block >= min_lines:
+                    block_count += 1
+                lines_in_block = 0  # Reset lines_in_block for a new block
+    # Check if the last block meets the criteria
+    if lines_in_block >= min_lines:
+        block_count += 1
+    return block_count - 1
